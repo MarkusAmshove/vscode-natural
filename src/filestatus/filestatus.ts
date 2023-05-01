f968ae3e3b46398ab7008b339ab03b5e47510327
Markus Amshove <scm@amshove.dev>
2023-05-01 16:23:53 +0200

    Highlight prototype stuff

---
+++ src/filestatus/filestatus.ts
@@ -0,0 +1,23 @@
+import { Event, ProviderResult, TreeDataProvider, TreeItem } from "vscode";
+
+export class FileStatusItem extends TreeItem {
+    constructor(public label: string) {
+        super(label)
+    }
+}
+
+export class FileStatusTreeProvider implements TreeDataProvider<FileStatusItem> {
+    onDidChangeTreeData?: Event<void | FileStatusItem | FileStatusItem[] | null | undefined> | undefined;
+
+    getTreeItem(element: FileStatusItem): TreeItem | Thenable<TreeItem> {
+        return element;
+    }
+
+    getChildren(element?: FileStatusItem | undefined): ProviderResult<FileStatusItem[]> {
+        if(!element) {
+            return [new FileStatusItem("Dirty files")];
+        }
+        return [];
+    }
+
+}
