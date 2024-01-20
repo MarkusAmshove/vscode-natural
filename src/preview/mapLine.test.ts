3c6c8fc94e0fec4056f66c178e3a5fcbd2795fe1
Markus Amshove <scm@amshove.dev>
2024-01-20 19:07:36 +0100

    Add tests for rendering

---
+++ src/preview/mapLine.test.ts
@@ -0,0 +1,67 @@
+import { MapLine } from "./mapLine";
+
+describe("MapLine should", () => {
+    it("render a line with some spaces", () => {
+        const sut = new MapLine();
+        sut.writeSpaces(5);
+        expect(sut.renderHtml())
+            .toEqual("<span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br/>");
+    });
+
+    it("render a line with a literal operand", () => {
+        const sut = new MapLine();
+
+        sut.writeOperand({
+            kind: "operand",
+            length: 5,
+            type: "literal",
+            operand: "Hi",
+            attributes: [],
+            id: 1,
+            sourceColumnEnd: 0,
+            sourceColumnStart: 0,
+            sourceLine: 0,
+        });
+
+        expect(sut.renderHtml())
+            .toEqual("<span id=\"element-1\" class=\"operand-literal\" title=>Hi&nbsp;&nbsp;&nbsp;</span><br/>");
+    });
+
+    it("render a line with a reference operand", () => {
+        const sut = new MapLine();
+
+        sut.writeOperand({
+            kind: "operand",
+            length: 5,
+            type: "reference",
+            operand: "#VAR",
+            attributes: [],
+            id: 1,
+            sourceColumnEnd: 0,
+            sourceColumnStart: 0,
+            sourceLine: 0,
+        });
+
+        expect(sut.renderHtml())
+            .toEqual("<span id=\"element-1\" class=\"operand-reference\" title=>#VAR&nbsp;</span><br/>");
+    });
+
+    it("cut an operand if its length is shorter than its value", () => {
+        const sut = new MapLine();
+
+        sut.writeOperand({
+            kind: "operand",
+            length: 2,
+            type: "reference",
+            operand: "#VAR",
+            attributes: [],
+            id: 1,
+            sourceColumnEnd: 0,
+            sourceColumnStart: 0,
+            sourceLine: 0,
+        });
+
+        expect(sut.renderHtml())
+            .toEqual("<span id=\"element-1\" class=\"cutoff operand-reference\" title=#VAR>#V</span><br/>");
+    });
+});
--- src/preview/preview.ts
+++ src/preview/preview.ts
@@ -2,6 +2,7 @@ import { basename } from "path";
 import * as vscode from "vscode";
 import { LanguageClient } from "vscode-languageclient/node";
 import { InputOperand, InputStructure } from "./previewTypes";
+import { MapLine } from "./mapLine";
 
 export function registerMapPreview(context: vscode.ExtensionContext, client: LanguageClient) {
     context.subscriptions.push(vscode.commands.registerCommand(
@@ -151,14 +152,14 @@ class MapPreview {
     }
 
     private renderPreview() {
-        const lines: InputLine[] = [];
-        let inputLine = new InputLine();
+        const lines: MapLine[] = [];
+        let inputLine = new MapLine();
 
         for (const element of this.structure.elements) {
             switch (element.kind) {
                 case "newline":
                     lines.push(inputLine);
-                    inputLine = new InputLine();
+                    inputLine = new MapLine();
                     break;
                 case "spaces":
                     inputLine.writeSpaces(element.spaces);
@@ -176,7 +177,7 @@ class MapPreview {
         const config = vscode.workspace.getConfiguration("natls");
         const pageSize = config.get<number>("maps.defaultPageSize", 24);
         for (let i = lines.length; i < pageSize; i++) {
-            lines.push(new InputLine());
+            lines.push(new MapLine());
         }
 
         return lines
@@ -336,81 +337,3 @@ body {
 
 }
 
-const SPACE_IN_VIEW = "&nbsp;"
-
-class InputLine {
-    private position: number = 1;
-    private content: string = "";
-    private length: number = 0;
-
-    reposition(column: number) {
-        const previousPosition = this.position;
-        const spacesToAdd = column - previousPosition;
-        if (spacesToAdd > 0) {
-            this.writeSpaces(spacesToAdd);
-        }
-    }
-
-    writeSpaces(spacesToAdd: number) {
-        this.length += spacesToAdd;
-        this.position += spacesToAdd;
-        this.content += `<span>${spaces(spacesToAdd)}</span>`;
-    }
-
-    writeOperand(operand: InputOperand) {
-        const content = operand.operand;
-        const length = operand.length;
-        const id = operand.id;
-
-        this.position += length;
-        this.length += length;
-        let contentToRender = content.substring(0, length);
-
-        let classes = [];
-        let title = "";
-        if (content.length > contentToRender.length) {
-            classes.push("cutoff");
-            title = content;
-        }
-
-        classes.push(`operand-${operand.type}`);
-
-        let isRightAligned = false;
-        for (const attribute of operand.attributes) {
-            if (attribute.kind === "AD") {
-                for (let i = 0; i < attribute.value.length; i++) {
-                    const value = attribute.value[i].toLowerCase();
-                    if (value === "r" || value === "z") {
-                        isRightAligned = true;
-                    }
-                    classes.push(`ad-${value}`);
-                }
-            }
-            // ....
-        }
-
-        if (contentToRender.length < length) {
-            const spacesToAdd = length - contentToRender.length;
-            const theSpaces = spaces(spacesToAdd);
-            if (isRightAligned) {
-                contentToRender = theSpaces + contentToRender;
-            } else {
-                contentToRender += theSpaces;
-            }
-        }
-
-        const idAssign = id ? `id="element-${id}"` : "";
-        this.content += `<span ${idAssign} class="${classes.join(" ")}" title=${title}>${contentToRender.replace(/ /g, SPACE_IN_VIEW)}</span>`;
-    }
-
-    renderHtml() {
-        if (this.content.length === 0) {
-            this.content = spaces(1);
-        }
-        return `${this.content}<br/>`;
-    }
-}
-
-function spaces(spaces: number): string {
-    return SPACE_IN_VIEW.repeat(spaces);
-}
